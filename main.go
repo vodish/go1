11a325ad3e21c4625a97de4a8865b13a011338ad
vodish <vodish@yandex.ru>
2024-01-27 21:26:41 +0300

    upd

--- main.go
+++ main.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Здарова, корова!")
+	// определите переменные ver, id, pi
+	// ver = v0.0.1 id = 0 pi = 3.1415.
+
+	var ver string = "v0.0.1"
+	var id int
+	pi := 3.1415
+
+	fmt.Println("ver =", ver, "id =", id, "pi =", pi)
 }
