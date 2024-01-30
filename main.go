4d0b4d4fbed123ed4bdeac7a6295ee487cb1c730
pkarasev <p.karasev@psw.ru>
2024-01-30 12:13:13 +0300

    upd

--- main.go
+++ main.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"time"
 )
 
+type Person struct {
+	Name        string
+	Email       string
+	dateOfBirth time.Time
+}
+
 func main() {
-	// bSlice := []byte(" \t\n a lone gopher \n\t\r\n")
-	str := `
-	
-	string  
-	
-	`
-	fmt.Println("[", strings.Trim(str, " \n\r\t"), "]") // a lone gopher
-	// fmt.Printf("%s", bSlice)                  // \t\n a lone gopher \n\t\r\n
+	p1 := Person{
+		Email: "Два",
+		Name:  "Один",
+	}
+	p2 := Person{"Один", "Два", time.Date(2000, 12, 1, 0, 0, 0, 0, time.UTC)}
+
+	fmt.Println(p1)
+	// fmt.Println(p2)
+	fmt.Printf("Man %#v", p2)
 
 }
